0b7d6e5cf00f28e7abba087c1d4f94b8dc374546
yukpiz <yukpiz@gmail.com>
2019-09-08 16:41:38 +0900

    updated

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/yukpiz/go-module-example/ex1"
 	"github.com/yukpiz/go-module-example/ex2"
 	"github.com/yukpiz/go-module-example/nst/ex3"
+	"github.com/yukpiz/go-module-example/other_branch/ex4"
 )
 
 func main() {
 	ex1.Ex1Hello() // ex1 hello!
 	ex2.Ex2Hello() // ex2 hello!
 	ex3.Ex3Hello() // ex3 hello!
+	ex4.Ex4Hello() // ex4 hello!
 }
